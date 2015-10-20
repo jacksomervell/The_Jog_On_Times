d33a1301195287c8228b991aff70fe89c68afdd4
Joshua Swiss <swissjoshua@gmail.com>
2015-10-20 11:13:54 +0100

    ui-router set up in app.js on frontend

---
+++ frontEnd/js/app.js
@@ -0,0 +1,15 @@
+angular
+	.module("jogOnTimesApp", ["ui.router"])
+	.config(MainRouter);
+
+	function MainRouter($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider.otherwise('/');
+	}
+
+	$stateProvider
+		.state('home', {
+			url: '/',
+			templateUrl: 'home.html'
+		})
+
+		
