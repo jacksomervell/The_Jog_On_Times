e89d262bef5f068110f20d788a2a0dc4ecc35987
Carryl Sorene <carrylsorene@gmail.com>
2015-10-20 18:36:12 +0100

    Fixed a few problems

--- frontEnd/js/headlinesController.js
+++ frontEnd/js/headlinesController.js
@@ -28,26 +28,17 @@ angular
     this.updateScore = function() {
       if (self.answer === self.getHeadlines[currentQuestion].blankWord) {
         score = score + 1;
-      console.log(self.getHeadlines[0].blankWord);
-      console.log('Correct so score is now ' + score);
+        console.log(self.getHeadlines[0].blankWord);
+        console.log('Correct so score is now ' + score);
       }
       else {
-      console.log(self.getHeadlines[currentQuestion].blankWord);
-      console.log('Wrong so score is still ' + score);
+        console.log(self.getHeadlines[currentQuestion].blankWord);
+        console.log('Wrong so score is still ' + score);
 
       }
 
       currentQuestion = currentQuestion + 1;
+      self.answer = '';
     }
 
-
   }
-
-
-// score starts at 0
-// if user input is same as blankWord in controllers/mainController
-//   then score +1
-
-// score rendered on score page and stored in localStorage
-
-// start of function, create var called question no = something in setup.html that tells you which question you're on
