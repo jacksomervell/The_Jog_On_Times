6762d99c6551700141c6af7df2962c5316d4c935
Joshua Swiss <swissjoshua@gmail.com>
2015-10-20 17:36:05 +0100

    name storing to object in localStorage

--- frontEnd/js/localStorageController.js
+++ frontEnd/js/localStorageController.js
@@ -6,9 +6,23 @@ angular
 		var self = this;
 
 		self.name = '';
+		self.jogOnApp = [];
 
-		self.saveName = function() {
+		this.saveName = function() {
+
+	
+			console.log(self.name)
 			
+			var object = { name: self.name };
+
+			self.jogOnApp = JSON.parse(localStorage.getItem('jogOnApp'));
+
+			if (!self.jogOnApp) {self.jogOnApp = []}
+
+			self.jogOnApp.push(object);
+
+			localStorage.setItem('jogOnApp', JSON.stringify(self.jogOnApp));
+
 		}
 	}
 
